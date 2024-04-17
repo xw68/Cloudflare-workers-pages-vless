068465378485003c47545b3f262ab56d7a4a6204
xw68 <164864533+xw68@users.noreply.github.com>
2024-04-17 18:20:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '77a571fb-4fd2-4b37-8596-1b7d9728bb5c';
+let userID = 'e54256ff-1710-42c3-bdf0-97bad6d2f596';
 
 const proxyIPs = ["workers.cloudflare.cyou"];
 
@@ -762,5 +762,5 @@ ${pvlesswstls}
   }
 }
 const cn_hostnames = [
-'' 
+'www.cctv.com' 
 ];
